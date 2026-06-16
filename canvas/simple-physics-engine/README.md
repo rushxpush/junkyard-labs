304616c12ecbd7c7e10a7fd58c3d244932f5b59a
Rafael Garcia <rushxpush@gmail.com>
2026-06-16 01:01:57 -0300

    🔨 wip: add box object creating on click
    - Create box objects on click and add to objects array
    - Add gravity to box objects
    - Remove box objects from array when the go out of bounds

---
+++ canvas/simple-physics-engine/README.md
@@ -0,0 +1,9 @@
+# Simple Physics Engine
+
+## TODO
+
+- ✅ Add Mouse coordinate mapping from viewport to canvas space
+  - ⚠ The logic could use canvas.getBoundingClientRect and I'm mixing local and global coordinates. I decided to let the current logic be. When/if the time comes I'll refactor the code to be more correct.
+- ✅ Create box on click
+- ✅ Apply gravity to box
+- ✅ Remove box object from program if it goes off screen
--- canvas/simple-physics-engine/main.js
+++ canvas/simple-physics-engine/main.js
@@ -1,3 +1,35 @@
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d");
+let lastTime = 0;
+const targetFPS = 60;
+const frameInterval = 1000 / targetFPS;
+const objects = [];
+const gravity = 1;
+const maxSpeed = 5;
+
+document.addEventListener('mouseup', () => {
+  createBox();
+});
+
+class Box {
+  x; 
+  y;
+  width;
+  height;
+  velocityX;
+  velocityY;
+
+  constructor(x = 0, y = 0, width = 50, height = 50) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.velocityX = 0;
+    this.velocityY = 0;
+  }
+
+}
+
 const canvasAttributes = {
   width: 800,
   height: 600,
@@ -11,11 +43,18 @@ const mouseAttributes = {
 document.addEventListener('mousemove', (event) => {
   mouseAttributes.x = event.x;
   mouseAttributes.y = event.y;
-  console.log(`{ x: ${mouseX()}, y: ${mouseY() }`);
 });
 
 
 const printCanvasData = () => {
+}
+
+const createBox = () => {
+  const width = 50;
+  const height = 50;
+  const x = mouseX() - width / 2;
+  const y = mouseY() - height / 2;
+  objects.push(new Box(x, y, width, height));
 }
 
 const mouseX = () => {
@@ -40,4 +79,50 @@ const mouseY = () => {
   return y;
 }
 
-const canvas = document.getElementById("canvas");
+const gameLoop = (currentTime) => {
+  requestAnimationFrame(gameLoop);
+
+  // Time elapsed since last frame
+  const deltaTime = currentTime - lastTime;
+
+  if (deltaTime >= frameInterval) {
+    // Adjust lastTime while accounting for residual latency drops
+    lastTime = currentTime - (deltaTime % frameInterval);
+
+    update(deltaTime);
+
+    render();
+  }
+}
+
+const update = (deltaTime) => {
+  for (let i = 0; i < objects.length; i++) {
+    calculateVelocity(objects, i);
+    calculatePosition(objects, i);
+    checkOutOfBounds(objects, i);
+  }
+}
+
+const checkOutOfBounds = (objects, index) => {
+  if (objects[index].y > canvas.height) objects.splice(index, 1);
+}
+
+const calculateVelocity = (objects, index) => {
+  if (objects[index].velocityY >= maxSpeed);
+  objects[index].velocityY += gravity;
+}
+
+const calculatePosition = (objects, index) => {
+  objects[index].y += objects[index].velocityY;
+}
+
+const render = () => {
+  ctx.beginPath();
+  ctx.clearRect(0, 0, 800, 600);
+  for (let i = 0; i < objects.length; i++) {
+    ctx.rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+  }
+  ctx.fill();
+}
+
+requestAnimationFrame(gameLoop);
